8e883c87a40321ea7bcb10b9cd105cf903996622
gusska158 <gusska158@gmail.com>
2026-01-04 11:12:52 +0900

    홈 화면 적립/ style module 생성 / 이미지 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { NextPage } from "next";
 
-export default function NotFound() {
+const NotFound: NextPage = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요</div>
+    <div>
+      <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
       <Link href="/search">검색</Link>
     </div>
   );
-}
+};
+
+export default NotFound;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import styles from "./page.module.css"; // css module
+// tailwind -> 호불호 심함
+// sass
+// styled components -> server component 에서 문제가 있음
+// vanilla extract -> windows 환경에서 잘 안됨
+
+import zLogo from "@/public/zlogo.png";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <>
+      <div className={styles.left}>
+        <Image src={zLogo} alt="" />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link className={styles.signup} href="/i/flow/signup">
+          계정 만들기
+        </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
+      </div>
+    </>
   );
 }
